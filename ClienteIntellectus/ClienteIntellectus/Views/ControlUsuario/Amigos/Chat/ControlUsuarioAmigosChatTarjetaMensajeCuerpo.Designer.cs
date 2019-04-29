320a713e225e826cce4d2268e925fb5a552a2a2f
Emilio Montiel <emilio.montiel@hotmail.com>
2019-04-28 21:52:48 -0500

    se creó el control de usuario tarjeta chat mensaje cuerpo

---
+++ ClienteIntellectus/ClienteIntellectus/Views/ControlUsuario/Amigos/Chat/ControlUsuarioAmigosChatTarjetaMensajeCuerpo.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace ClienteIntellectus.Views.ControlUsuario.Amigos.Chat
+{
+    partial class ControlUsuarioAmigosChatTarjetaMensajeCuerpo
+    {
+        /// <summary> 
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de componentes
+
+        /// <summary> 
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureAvatar = new System.Windows.Forms.PictureBox();
+            this.labelNick = new System.Windows.Forms.Label();
+            this.labelMensaje = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureAvatar)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureAvatar
+            // 
+            this.pictureAvatar.Location = new System.Drawing.Point(6, 7);
+            this.pictureAvatar.Name = "pictureAvatar";
+            this.pictureAvatar.Size = new System.Drawing.Size(65, 50);
+            this.pictureAvatar.TabIndex = 0;
+            this.pictureAvatar.TabStop = false;
+            // 
+            // labelNick
+            // 
+            this.labelNick.AutoSize = true;
+            this.labelNick.Location = new System.Drawing.Point(84, 7);
+            this.labelNick.Name = "labelNick";
+            this.labelNick.Size = new System.Drawing.Size(32, 13);
+            this.labelNick.TabIndex = 1;
+            this.labelNick.Text = "NICK";
+            // 
+            // labelMensaje
+            // 
+            this.labelMensaje.AutoSize = true;
+            this.labelMensaje.Location = new System.Drawing.Point(79, 26);
+            this.labelMensaje.MaximumSize = new System.Drawing.Size(225, 0);
+            this.labelMensaje.Name = "labelMensaje";
+            this.labelMensaje.Size = new System.Drawing.Size(46, 13);
+            this.labelMensaje.TabIndex = 2;
+            this.labelMensaje.Text = "mensaje";
+            // 
+            // ControlUsuarioAmigosChatTarjetaMensajeCuerpo
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.Controls.Add(this.labelMensaje);
+            this.Controls.Add(this.labelNick);
+            this.Controls.Add(this.pictureAvatar);
+            this.Name = "ControlUsuarioAmigosChatTarjetaMensajeCuerpo";
+            this.Padding = new System.Windows.Forms.Padding(0, 0, 0, 2);
+            this.Size = new System.Drawing.Size(316, 62);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureAvatar)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureAvatar;
+        private System.Windows.Forms.Label labelNick;
+        private System.Windows.Forms.Label labelMensaje;
+    }
+}
---
+++ ClienteIntellectus/ClienteIntellectus/Views/ControlUsuario/Amigos/Chat/ControlUsuarioAmigosChatTarjetaMensajeCuerpo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ClienteIntellectus.Views.ControlUsuario.Amigos.Chat
+{
+    public partial class ControlUsuarioAmigosChatTarjetaMensajeCuerpo : UserControl
+    {
+        public ControlUsuarioAmigosChatTarjetaMensajeCuerpo()
+        {
+            InitializeComponent();
+        }
+    }
+}
